b588497d556e10699f93e559d19dec1f831aa942
Eliott K Cassidy <cassid1@stolaf.edu>
2020-06-30 20:56:47 -0600

    test module baby

---
+++ testmodule/bleh.cpp
@@ -0,0 +1,5 @@
+#include "bleh.h"
+
+int addEliott(int a, int b){
+    return a + b;
+}
---
+++ testmodule/bleh_driver.cpp
@@ -0,0 +1,9 @@
+#include "bleh.cpp"
+int main(){
+    printf("Hello World !!!!!!!\n");
+    int first = 2;
+    int second = 3;
+    int returnVal = 0;
+    returnVal = addEliott(first, second);
+    printf("Hello World\n");
+}
