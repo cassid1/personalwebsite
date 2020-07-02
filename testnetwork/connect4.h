352a99ac1fbcaf3906c43ec0a65698eb6c6a1fe3
Eliott K Cassidy <cassid1@stolaf.edu>
2020-07-02 12:23:46 -0600

    Update connect4.h

--- testnetwork/connect4.h
+++ testnetwork/connect4.h
@@ -70,11 +70,11 @@ class GameBoard{
         initializeGameBoard();
 
         //loads the two networks
-        //Network learner("connect4black.net");
-        //Network learner2("connect4red.net");
-	const vector<int> dimentions = {126,75,40,7};
-	Network learner(dimentions, false, true);
-	Network learner2(dimentions, false, true);
+        Network learner("connect4black.net");
+        Network learner2("connect4red.net");
+	//const vector<int> dimentions = {126,75,40,7};
+	//Network learner(dimentions, false, true);
+	//Network learner2(dimentions, false, true);
         learnerP = &(learner);
         learner2P = &(learner2);
 
