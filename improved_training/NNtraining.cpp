f465183929c5a30292ca9744fe6f512b67074907
Eliott Cassidy <cassid1@stolaf.edu>
2020-07-25 19:39:13 -0500

    training1

---
+++ improved_training/NNtraining.cpp
@@ -0,0 +1,40 @@
+
+#include"connect4.h"
+
+int main(){
+    srand(time(NULL));
+    //bool compGoesFirst = false;
+    // other dim {7*6*3,100,75,50, 7};
+    const vector<int> dimensions = {7*6*3,75,40, 7};
+    GameBoard a;
+    //a.playUserGame(compGoesFirst);
+    int totalBlackWins = 0;
+    int totalRedWins = 0;
+    int totalVertWins = 0;
+    int totalDiagWins = 0;
+    int totalHorizWins = 0;
+    int totalChoices = 0;
+    int totalMoves = 0;
+    for(int i=0; i<10; i++){
+        a.playCPUGame();
+
+        totalBlackWins += a.getBlackWins();
+        totalRedWins += a.getRedWins();
+
+        totalDiagWins += a.getDiagonalWins();
+        totalVertWins += a.getVerticalWins();      
+        totalHorizWins += a.getHorizontalWins(); 
+
+        totalChoices += a.getNumChoices();
+        totalMoves += a.getMoveNumber();
+    }
+    cout<<"Black (O) Wins: "<<totalBlackWins<<endl;
+    cout<<"Red (X) Wins: "<<totalRedWins<<"\n"<<endl;
+    cout<<"Vertical Wins: "<<totalVertWins<<endl;    
+    cout<<"Horizontal Wins: "<<totalHorizWins<<endl;
+    cout<<"Diagonal Wins: "<<totalDiagWins<<"\n"<<endl;
+    cout<<"Total Selections: "<<totalChoices<<endl;
+    cout<<"Total Moves: "<<totalMoves<<endl; 
+    
+    return 1;
+}
---
+++ improved_training/connect4.h
@@ -0,0 +1,91 @@
+#ifndef _CONNECT_4_
+#define _CONNECT_4_
+
+#include "Neural_net.h"
+
+using namespace std;
+
+class GameBoard{
+    //used for calling any functions of the networks within a game
+    Network* learnerP;
+    Network* learner2P;
+
+//three two dimentional arrays, representing the state of each slot of the 42 slot board
+//first dimention is column, second dimension is row
+    bool isEmpty[7][6];
+    bool isRed[7][6];
+    bool isBlack[7][6];
+//keep track of which of the 7 columns are available (true) and which of the 7 are full (false)
+    bool availableSelections[7];
+//number of open spaces
+    int open;
+//what move is it within the game
+    int moveNumber;
+// did black win?
+    bool blackWon;
+// is the game over?
+    bool isGameOver;
+// is it blacks turn?
+    bool blacksMove;
+//was the game a draw?
+    bool isDraw;
+//used for backpropagation, these have the first dimension of 42 corresponding to as many of the 42 possible moves in a game as necessary,
+//stores the input for network, what the network ended up choosing, and what it could have chosen from
+    int choiceStorage[42];
+    vector<float> inputStorage[42];
+    bool availableStorage [42][7];
+
+//used for summary statistics
+    int numDiagonalWins;
+  int numVerticalWins;
+  int numHorizontalWins;
+  int numBlackWins;
+  int numRedWins;
+  int numChoices;
+
+    public:
+        GameBoard(){
+            initializeGameBoard();
+        }
+
+//returning summary statistics from a game
+  int getBlackWins(){return numBlackWins;}
+  int getRedWins(){return numRedWins;}
+  int getVerticalWins(){return numVerticalWins;}
+  int getDiagonalWins(){return numDiagonalWins;}
+  int getHorizontalWins(){return numHorizontalWins;}
+  int getMoveNumber(){return moveNumber;}
+  int getNumChoices(){return numChoices;}
+
+  void createBlankFile(string fileName);
+
+//functions to play a game with
+  void initializeGameBoard();
+  void displayCurrentGameBoard();
+  void makeMove(int c);
+  int makeTurn();
+  void updateNetworks();
+
+  void playCPUGame(){
+        initializeGameBoard();
+
+        //loads the two networks
+        const vector<int> dimensions = {7*6*3,75,40, 7};
+        Network learner("connect4black.net");
+        Network learner2("connect4red.net");
+        //Network learner(dimensions);
+        //Network learner2(dimensions);
+        learnerP = &(learner);
+        learner2P = &(learner2);
+
+        while(!isGameOver){
+        makeTurn();
+        moveNumber++;
+        }
+	    displayCurrentGameBoard();
+        updateNetworks();
+    }
+
+};
+
+#endif // _CONNECT_4_
